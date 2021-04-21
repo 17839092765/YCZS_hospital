2414f59756799f8d8926de0ff2b21e3374e8b6c0
fengxiangxin <227306865@qq.com>
2021-04-21 16:07:07 +0800

    -

--- src/views/fivebutton/index.vue
+++ src/views/fivebutton/index.vue
@@ -69,6 +69,24 @@ export default {
   methods: {
     interactive(active) {
       this.active = active;
+      switch (active) {
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          break;
+        case 4:
+          break;
+        case 5:
+          break;
+        case 6:
+          break;
+        case 7:
+          break;
+        default:
+          break;
+      }
     },
   },
   created() {},
