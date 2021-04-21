818b620363f231628c4cffa5a4c69d35e202d6a0
张祥 <1341307180@qq.com>
2021-04-21 15:48:17 +0800

    123

--- src/views/fivebutton/index.vue
+++ src/views/fivebutton/index.vue
@@ -1,12 +1,59 @@
 <template>
-  <div class="fivebutton"></div>
+  <div class="fivebutton">
+    <div class="case" v-for="(item, index) in btnlist" :key="index">
+      {{ item.name }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      btnlist: [{}],
+      btnlist: [
+        {
+          name: "医院报警系统",
+          key: 1,
+          path: "",
+          clickpath: "",
+        },
+        {
+          name: "视频监控系统",
+          key: 2,
+          path: "",
+          clickpath: "",
+        },
+        {
+          name: "人脸识别",
+          key: 3,
+          path: "",
+          clickpath: "",
+        },
+        {
+          name: "火灾报警系统",
+          key: 4,
+          path: "",
+          clickpath: "",
+        },
+        {
+          name: "门禁管理系统",
+          key: 5,
+          path: "",
+          clickpath: "",
+        },
+        {
+          name: "区域客流分析系统",
+          key: 6,
+          path: "",
+          clickpath: "",
+        },
+        {
+          name: "梯控",
+          key: 7,
+          path: "",
+          clickpath: "",
+        },
+      ],
     };
   },
   computed: {},
@@ -28,7 +75,7 @@ export default {
 <style lang="scss" scoped>
 .fivebutton {
   position: absolute;
-  left: 4.5rem;
+  left: 5rem;
   bottom: 1rem;
   width: 1rem;
   height: 4rem;
